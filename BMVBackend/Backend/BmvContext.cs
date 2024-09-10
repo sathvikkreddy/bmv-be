9c081be147ad8cb310f42179a88fff1e081a14fb
dniketh <dniketh@actalentservices.com>
2024-09-10 09:50:47 +0000

    Added the Boiler Plate code for Backend

---
+++ BMVBackend/Backend/BmvContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend
+{
+    public class BmvContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"server=.\sqlexpress;initial catalog=bmv;user id=sa;password=Pass@123;trustservercertificate=true");
+        }
+    }
+}
